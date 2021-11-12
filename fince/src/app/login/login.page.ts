02a5ebdc6422c4a66714f570ce68de26cf63c732
Vict-vargass <vict.vargass@duocuc.cl>
2021-11-11 22:34:04 -0300

    metodo entrar, validado

--- fince/src/app/login/login.page.ts
+++ fince/src/app/login/login.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CrudStorageService } from '../crud-storage.service';
+import { AlertController } from '@ionic/angular';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -7,9 +9,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private crud: CrudStorageService,
+    private alertController: AlertController,
+    private router: Router
+  ) { }
+    listado=[]
   ngOnInit() {
+  }
+
+  async entrar(correo: HTMLInputElement, contra: HTMLInputElement)
+  {
+    const valor_correo = await this.crud.get(correo.value)
+    if (correo.value.length==0 || contra.value.length== 0 ){
+      const alert = await this.alertController.create({
+        cssClass: 'my-custom-class',
+        header: 'Campos requeridos',
+        message: 'Complete todos los campos para realizar el registro',
+        buttons: [
+          {
+            text: 'OK',
+            role: 'cancel'
+          }
+        ]
+      });
+      await alert.present();
+    }else{
+      if (valor_correo !=null && valor_correo.length >0 ){
+        this.listado = valor_correo
+        if(this.listado[0].correo == correo.value && this.listado[0].contrasenia == contra.value){
+          this.router.navigate(['/home'])
+        }else{
+          const alert = await this.alertController.create({
+            cssClass: 'my-custom-class',
+            header: 'Correo o contraseña invalidos',
+            message: 'Por favor, ingrese nuevamente su correo y contraseña',
+            buttons: [
+              {
+                text: 'OK',
+                role: 'cancel'
+              }
+            ]
+          });
+          await alert.present();
+        }
+
+      }
+    }
   }
 
 }
